c0ac90e091e7149714ca2979f49076eb5aa7edd4
Ron Rider Jr <rideron89@hey.com>
2023-06-23 16:36:44 -0400

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-env node */
+
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh"],
+  rules: {
+    "indent": ["warn", 2],
+    "react-refresh/only-export-components": "warn",
+  },
+};
