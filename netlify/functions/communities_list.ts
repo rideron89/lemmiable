f053df81d667fd16e108bae16cbfb9b315f4e01c
Ron Rider <rideron89@hey.com>
2023-06-24 09:39:41 -0400

    add types to and refactor communities_list function

--- netlify/functions/communities_list.ts
+++ netlify/functions/communities_list.ts
@@ -2,6 +2,54 @@ import type { Handler, HandlerEvent, HandlerResponse } from "@netlify/functions"
 import axios from "axios";
 import { ErrInvalidRequestResponse, JsonOkResponse } from "../../lib/response";
 
+interface CommunityInfo {
+  id: number
+  name: string
+  title: string
+  removed: boolean
+  published: string
+  deleted: boolean
+  nsfw: boolean
+  actor_id: string
+  local: boolean
+  hidden: boolean
+  posting_restricted_to_mods: boolean
+  instance_id: number
+}
+
+type CommunitySubscriptionStatus = "NotSubscribed" | "Subscribed"
+
+interface CommunitySubscriptionData {
+  subscribed: CommunitySubscriptionStatus
+}
+
+interface CommunityBlockedData {
+  blocked: boolean
+}
+
+interface CommunityCounts {
+  id:  number
+  community_id:  number
+  subscribers:  number
+  posts:  number
+  comments:  number
+  published: string
+  users_active_day: number
+  users_active_week: number
+  users_active_month: number
+  users_active_half_year: number
+  hot_rank: number
+}
+
+type RawCommunity = CommunitySubscriptionData & CommunityBlockedData & {
+  community: CommunityInfo
+  counts: CommunityCounts
+}
+
+type FormattedCommunity = CommunityBlockedData & CommunityInfo & CommunitySubscriptionData & {
+  counts: CommunityCounts
+}
+
 const handler: Handler = async (event: HandlerEvent): Promise<HandlerResponse> => {
   const forwardedIp: string | undefined = event.headers["x-forwarded-for"]
   if (!forwardedIp) {
@@ -21,7 +69,12 @@ const handler: Handler = async (event: HandlerEvent): Promise<HandlerResponse> =
 
   switch (response.status) {
   case 200:
-    return new JsonOkResponse(response.data).build()
+    return new JsonOkResponse(response.data.communities.map((c: RawCommunity) => ({
+      ...c.community,
+      blocked: c.blocked,
+      subscribed: c.subscribed,
+      counts: c.counts,
+    }))).build()
   case 418:
     console.error(`status ${response.status}; ${await response.data}`)
     return new ErrInvalidRequestResponse("invalid request").build()
